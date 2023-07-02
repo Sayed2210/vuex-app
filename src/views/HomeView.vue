34afcbda0c85be5efb2ba47f1bb8978e01f47b4c
Elsayed Hassan <eh600039@gmail.com>
2023-07-02 17:20:18 +0300

    finish

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="home">
     <h1>This is an Home page</h1>
-    <h2>My Name is {{ this.$store.state.name }}</h2>
-    <h2>My Age is {{ this.$store.state.age }}</h2>
-    <input type="text" name="name" v-model="this.$store.state.name" />
-    <h3>{{ this.$store.getters.yourData }}</h3>
+    <h2>My Name is {{ getProducts.name }}</h2>
+    <h2>My Age is {{ getProducts.age }}</h2>
+    <input type="text" name="name" v-model="getProducts.name" />
+    <h3>{{ getters.yourData }}</h3>
+    <button @click="store.commit('changeTitle')">change name</button>
     <ul>
-      <li
-        v-for="product in this.$store.state.getProducts.products"
-        :key="product.id"
-      >
+      <li v-for="product in getProducts.products" :key="product.id">
         {{ product.title }}
       </li>
     </ul>
-    <button @click="this.$store.dispatch('getDataFromApi')">
-      Get Products
-    </button>
+    <button @click="store.dispatch('getDataFromApi')">Get Products</button>
   </div>
 </template>
 
-<script></script>
+<!-- compstion  api-->
+<script setup>
+import { useStore } from "vuex";
+const store = useStore();
+const getters = store.getters;
+const getProducts = store.state.getProducts;
+</script>
+
+<!-- option api -->
+<!-- <script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+export default {
+  computed: {
+    ...mapState(["getProducts"]),
+    ...mapGetters(["yourData"]),
+  },
+  methods: {
+    ...mapMutations(["changeTitle"]),
+    ...mapActions(["getDataFromApi"]),
+    async getProductsFun() {
+      await this.getDataFromApi();
+    },
+    changName() {
+      this.changeTitle();
+    },
+  },
+};
+</script> -->
